45455bef37f7124369e9c3def783840c231dc826
YBPrasad <ybuddhikaprasad@gmail.com>
2021-10-13 02:06:05 +0530

    service modify

--- src/main/java/com/example/StudentCrud/service/StudentService.java
+++ src/main/java/com/example/StudentCrud/service/StudentService.java
@@ -2,9 +2,12 @@ package com.example.StudentCrud.service;
 
 
 import com.example.StudentCrud.domain.Student;
+
 import com.example.StudentCrud.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 
@@ -15,17 +18,30 @@ public class StudentService {
     private StudentRepository repo;
 
     public List<Student> listAll(){
-        return repo.findAll();
+
+        return this.repo.findAll();
     }
 
-    public void save(Student std){
-        repo.save(std);
+    public Student save(Student std){
+
+        return this.repo.save(std);
     }
 
-    public Student get(long id){
-        return (Student) repo.findById(id).get();
+    public Student getUserById(long id){
+
+        return this.repo.findById(id).orElseThrow(()->new ResourceAccessException("Student not found with this id "+id));
     }
-    public void delete(long id){
-        repo.deleteById(id);
+    public ResponseEntity<Student> delete(long id){
+        Student existingStudent=this.repo.findById(id).orElseThrow(()->new ResourceAccessException("Student not found with this id "+id));
+        this.repo.delete(existingStudent);
+        return ResponseEntity.ok().build();
+    }
+
+    public Student updateStudent(Student std,long id){
+        Student existingStudent=this.repo.findById(id).orElseThrow(()->new ResourceAccessException("Student not found with this id "+id));
+        existingStudent.setStudentName(std.getStudentName());
+        existingStudent.setCourse(std.getCourse());
+        existingStudent.setFee(std.getFee());
+        return this.repo.save(existingStudent);
     }
 }
